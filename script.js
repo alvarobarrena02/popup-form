65012b827eda5263fcd21ffb0ee36da290b6f349
Alvaro <alvaro.barrena@campusfp.es>
2024-05-08 12:24:04 +0200

    Versión 1: Diseño del formulario COMPLETADO

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ jQuery(document).ready(function($) {
                 '<h2>Contacta con nosotros</h2>' +
                 '<span id="cerrar-popup">×</span>' +
                 '<form id="contact-form">' +
-                '<label for="nombre" class="fcustom">Nombre: <input type="text" id="nombre" name="nombre"></label>' +
-                '<label for="apellidos" class="fcustom">Apellidos: <input type="text" id="apellidos" name="apellidos"></label>' +
-                '<label for="email" class="fcustom">Email: <input type="email" id="email" name="email"></label>' +
-                '<label for="telefono" class="fcustom">Teléfono: <input type="text" id="telefono" name="telefono"></label>' +
-                '<label for="asunto" class="fcustom-full">Asunto: <input type="text" id="asunto" name="asunto"></label>' +
-                '<label for="mensaje" class="fcustom-full">Mensaje: <textarea id="mensaje" name="mensaje"></textarea></label>' +
+                '<label for="nombre" class="fcustom">Nombre <input type="text" id="nombre" name="nombre"></label>' +
+                '<label for="apellidos" class="fcustom">Apellidos <input type="text" id="apellidos" name="apellidos"></label>' +
+                '<label for="email" class="fcustom">Email <input type="email" id="email" name="email"></label>' +
+                '<label for="telefono" class="fcustom">Teléfono <input type="text" id="telefono" name="telefono"></label>' +
+                '<label for="asunto" class="fcustom-full">Asunto <input type="text" id="asunto" name="asunto"></label>' +
+                '<label for="mensaje" class="fcustom-full">Mensaje <textarea id="mensaje" name="mensaje"></textarea></label>' +
                 '<label for="politica-privacidad" class="acepto"><input type="checkbox" id="politica-privacidad" name="politica-privacidad"> Acepto la Política de Privacidad y que mis datos serán almacenados para esta solicitud GDPR.</label>' +
                 '<label for="consentimiento-datos" class="acepto"><input type="checkbox" id="consentimiento-datos" name="consentimiento-datos"> Consiente el tratamiento de sus datos personales con la finalidad de enviarle invitaciones a cursos y talleres similares al solicitado.</label>' +
                 '<input type="submit" value="Enviar"">' +
